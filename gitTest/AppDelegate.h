72c45c7f3a9e70742aa3a5fb4dff37bdc6f58705
kino <1253109318@qq.com>
2020-01-13 17:48:03 +0800

    Initial Commit

---
+++ gitTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  gitTest
+//
+//  Created by kino on 2020/1/13.
+//  Copyright © 2020 kino. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ gitTest/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  gitTest
+//
+//  Created by kino on 2020/1/13.
+//  Copyright © 2020 kino. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
